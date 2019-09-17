d6df7bceaf479dbefd7804232ef8ba41889c5fd9
Tim Head <betatim@gmail.com>
2019-09-17 22:33:30 +0200

    Increase timeout

--- jupyter_server_proxy/handlers.py
+++ jupyter_server_proxy/handlers.py
@@ -286,7 +286,7 @@ class ProxyHandler(WebSocketHandlerMixin, IPythonHandler):
     def proxy_request_options(self):
         '''A dictionary of options to be used when constructing
         a tornado.httpclient.HTTPRequest instance for the proxy request.'''
-        return dict(follow_redirects=False)
+        return dict(follow_redirects=False, request_timeout=600)
 
     def check_xsrf_cookie(self):
         '''
